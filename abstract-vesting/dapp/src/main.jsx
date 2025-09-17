f38b196ef8b2ed554eeacc3c7cc219b699679a82
Mfer Vestside <97671389+Constitosh@users.noreply.github.com>
2025-09-17 12:21:45 +0200

    Update main.jsx

--- abstract-vesting/dapp/src/main.jsx
+++ abstract-vesting/dapp/src/main.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-
-// AGW provider + chain
 import { AbstractWalletProvider } from '@abstract-foundation/agw-react'
-import { abstract } from 'viem/chains' // mainnet
+import { abstract } from 'viem/chains'
 
 createRoot(document.getElementById('root')).render(
   <AbstractWalletProvider chain={abstract}>
