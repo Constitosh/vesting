229bfee37885a01061098f88d2ffd8b80fbc2753
Mfer Vestside <97671389+Constitosh@users.noreply.github.com>
2025-09-17 11:45:43 +0200

    Update main.jsx

--- abstract-vesting/dapp/src/main.jsx
+++ abstract-vesting/dapp/src/main.jsx
@@ -1,18 +1,4 @@
-// dapp/src/main.jsx
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-
-// AGW + Query setup
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { AgwProvider } from '@abstract-foundation/agw-react';
-
-const qc = new QueryClient();
-
-createRoot(document.getElementById('root')).render(
-  <QueryClientProvider client={qc}>
-    <AgwProvider>
-      <App />
-    </AgwProvider>
-  </QueryClientProvider>
-);
+createRoot(document.getElementById('root')).render(<App />);
