300f94eabe60335869e3039229718d89b95134c3
Mfer Vestside <97671389+Constitosh@users.noreply.github.com>
2025-09-17 12:34:35 +0200

    Update main.jsx

--- abstract-vesting/dapp/src/main.jsx
+++ abstract-vesting/dapp/src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+
+// Official provider (brings wagmi + TanStack Query under the hood)
 import { AbstractWalletProvider } from '@abstract-foundation/agw-react'
-import { abstract } from 'viem/chains'
+import { abstract } from 'viem/chains' // Abstract mainnet
 
 createRoot(document.getElementById('root')).render(
   <AbstractWalletProvider chain={abstract}>
